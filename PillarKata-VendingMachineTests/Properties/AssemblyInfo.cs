5a70931bfdb6e23f00b4d7a63837e00350aa95b3
Ed Brady <ed@ebrady.net>
2018-06-07 05:44:11 -0400

    PillarKata-VendingMachine: Initial Repo Creation

---
+++ PillarKata-VendingMachineTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PillarKata-VendingMachineTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PillarKata-VendingMachineTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70461a29-318d-4c64-8dbe-b9eb77f82ae4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
