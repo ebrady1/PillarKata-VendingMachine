a1b8c62c470d938dafd03648e3890a0b08d66cda
Ed Brady <ed@ebrady.net>
2018-06-09 10:39:17 -0400

    Vending Machine Kata:  Implementation of the InsertCoin method/use case

--- PillarKata-VendingMachine/PillarKata-VendingMachine/CoinChanger.cs
+++ PillarKata-VendingMachine/PillarKata-VendingMachine/CoinChanger.cs
@@ -2,20 +2,100 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PillarKata_VendingMachine
 {
-    /*
-        The CoinChanger class 
-        This class implements coin related functions
-    */
+    /// <summary>
+    /// The <c>Coin</c> class.
+    /// Contains all methods to describe a coin
+    /// </summary>
+    /// 
+    public class Coin
+    {
+        public Coin()
+        {
+            Value = 0;
+            CoinName = "";
+        }
+
+        public Coin(String name, UInt16 value)
+        {
+            Value = value;
+            CoinName = name; 
+        }
+
+        public UInt16 Value { get; set; } 
+        public String CoinName { get; set; }
+    };
+
+    /// <summary>
+    /// <c>CoinChangerEventArgs</c> class.
+    /// EventArgument for Coin Inserted Event 
+    /// </summary>
+    /// 
+    public class CoinChangerEventArgs : EventArgs
+    {
+        public UInt16 Value { get; set; }
+    };
+
+
     /// <summary>
     /// The main <c>CoinChanger</c> class.
     /// Contains all methods for performing coin related functions.
     /// </summary>
     public class CoinChanger
     {
+        //The current amount of change in the coin changer.
+        UInt16 m_Nickels        = 0;
+        UInt16 m_Dimes          = 0;
+        UInt16 m_Quarters       = 0;
+        UInt16 m_HalfDollars    = 0;
+        UInt16 m_Dollars        = 0;
+
+        //Known Coin types
+        Dictionary<string, Coin> COIN_VALUES = 
+            new Dictionary<string, Coin> 
+        {
+            { "NICKEL",         new Coin("NICKEL",      5) },
+            { "DIME",           new Coin("Dime",        10) },
+            { "QUARTER",        new Coin("Quarter",     25) },
+            { "HALF DOLLAR",    new Coin("Half Dollar", 50) },
+            { "DOLLAR",         new Coin("Dollar",      100) }
+        };
+
+        //The number of coins currently in the coin changer
+        static Dictionary<Coin, UInt16> m_coinVault;
+
+        //Declare a callback for others to know when a coin has been inserted
+        public delegate void CoinEvent(UInt16 value);
+
+        public event EventHandler CoinInserted;
+
+        /// <summary>
+        /// Class Constructor
+        /// </summary>
+        public CoinChanger()
+        {
+            COIN_VALUES.Add("5", COIN_VALUES["NICKEL"]);
+            COIN_VALUES.Add("10", COIN_VALUES["DIME"]);
+            COIN_VALUES.Add("25", COIN_VALUES["QUARTER"]);
+            COIN_VALUES.Add("50", COIN_VALUES["HALF DOLLAR"]);
+            COIN_VALUES.Add("100", COIN_VALUES["DOLLAR"]);
+
+            //The number of coins contained in the Coin Changer Vault
+            m_coinVault = 
+                new Dictionary<Coin, UInt16>
+            {
+                { COIN_VALUES["NICKEL"],       m_Nickels },
+                { COIN_VALUES["DIME"],         m_Dimes },
+                { COIN_VALUES["QUARTER"],      m_Quarters},
+                { COIN_VALUES["HALF DOLLAR"],   m_HalfDollars},
+                { COIN_VALUES["DOLLAR"],       m_Dollars}
+            };
+        }
+        
         /// <summary>
         /// Inserts a coin into the coin changer and test if it is a valid denomination. 
         /// </summary>
@@ -25,20 +105,41 @@ namespace PillarKata_VendingMachine
         /// <param name="coin">A string representing a valid coin denomination"</param>
         public bool InsertCoin(String coin)
         {
-            return false;
+            bool retVal = false;
+            UInt16 vaultItemCount; 
+            Coin value;
+
+            if ((!String.IsNullOrWhiteSpace(coin)) && 
+                (!String.IsNullOrEmpty(coin)))
+            {
+                //Remove any Whitespace
+                Regex.Replace(coin, @"\s+", "");
+
+                //Go UpperCase
+                coin = coin.ToUpper();
+
+                // We expect the parameter to be in the form of a string
+                if (COIN_VALUES.TryGetValue(coin, out value))
+                {
+                    if (m_coinVault.TryGetValue(value, out vaultItemCount))
+                    {
+                        //Increment the value in the vault
+                        vaultItemCount++;
+
+                        CoinChangerEventArgs e = new CoinChangerEventArgs();
+                        e.Value = value.Value;
+
+                        //Throw an event here
+                        CoinInserted(this, e);
+
+                        //Report success
+                        retVal = true;
+                    }
+                }
+            }
+            return retVal;
         }
         
-        /// <summary>
-        /// Inserts a coin into the coin changer and test if it is a valid denomination. 
-        /// </summary>
-        /// <returns>
-        /// true if the coin is valid, otherwise false/
-        /// </returns>
-        /// <param name="coin">A integer representing a valid coin denomination"</param>
-        public bool InsertCoin(int coin)
-        {
-            return false;
-        }
 
         /// <summary>
         /// Dispense change after a valid transaction or refund. 
--- PillarKata-VendingMachine/PillarKata-VendingMachineTests/CoinChangerTests.cs
+++ PillarKata-VendingMachine/PillarKata-VendingMachineTests/CoinChangerTests.cs
@@ -14,6 +14,11 @@ namespace PillarKata_VendingMachineTests
             lastCoinValue = 0;
         }
 
+        void CoinInsertedEvent(object sender, EventArgs e)
+        {
+            lastCoinValue = ((CoinChangerEventArgs)e).Value;
+        }
+
         /// <summary>
         /// Tests variations of valid/invalid denominations for the CoinChanger 
         /// </summary>
@@ -22,6 +27,7 @@ namespace PillarKata_VendingMachineTests
         {
             //Create a new CoinChanger object
             CoinChanger coinChanger = new CoinChanger();
+            coinChanger.CoinInserted += CoinInsertedEvent;
 
             //Try various "valid" coing methods
             Assert.AreEqual(true, coinChanger.InsertCoin("Nickel"), "Nickel not detected correctly");
@@ -30,7 +36,7 @@ namespace PillarKata_VendingMachineTests
             Assert.AreEqual(10, lastCoinValue, "Dime coin value incorrect");
             Assert.AreEqual(true, coinChanger.InsertCoin("Quarter"), "Quarter not detected correctly");
             Assert.AreEqual(25, lastCoinValue, "Quarter coin value incorrect");
-            Assert.AreEqual(true, coinChanger.InsertCoin("Half Dollar"), "Half Dollarnot detected correctly");
+            Assert.AreEqual(true, coinChanger.InsertCoin("Half Dollar"), "Half Dollar not detected correctly");
             Assert.AreEqual(50, lastCoinValue, "Half Dollar coin value incorrect");
             Assert.AreEqual(true, coinChanger.InsertCoin("Dollar"), "Dollar not detected correctly");
             Assert.AreEqual(100, lastCoinValue, "Dollar coin value incorrect");
@@ -52,40 +58,16 @@ namespace PillarKata_VendingMachineTests
             Assert.AreEqual(true, coinChanger.InsertCoin("100"), "Dollar not detected correctly");
             Assert.AreEqual(100, lastCoinValue, "Dollar coin value incorrect");
 
-            Assert.AreEqual(true, coinChanger.InsertCoin(5), "Nickel not detected correctly");
-            Assert.AreEqual(5, lastCoinValue, "Nickel coin value incorrect");
-            Assert.AreEqual(true, coinChanger.InsertCoin(10), "Dime not detected correctly");
-            Assert.AreEqual(10, lastCoinValue, "Dime coin value incorrect");
-            Assert.AreEqual(true, coinChanger.InsertCoin(25), "Quarter not detected correctly");
-            Assert.AreEqual(25, lastCoinValue, "Quarter coin value incorrect");
-            Assert.AreEqual(true, coinChanger.InsertCoin(50), "Half Dollarnot detected correctly");
-            Assert.AreEqual(50, lastCoinValue, "Half Dollar coin value incorrect");
-            Assert.AreEqual(true, coinChanger.InsertCoin(100), "Dollar not detected correctly");
-            Assert.AreEqual(100, lastCoinValue, "Dollar coin value incorrect");
-
-
             Assert.AreEqual(false, coinChanger.InsertCoin("Penny"), "Invalid coin detected as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("1"), "Invalid coin detected as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
-            Assert.AreEqual(false, coinChanger.InsertCoin(1), "Invalid coin detected as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
 
             Assert.AreEqual(false, coinChanger.InsertCoin(""), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("5 Centavo"), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("10 Kopecks"), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("Rouble"), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("5 Roubles"), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("Talon"), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
             Assert.AreEqual(false, coinChanger.InsertCoin("Good Looks"), "Invalid coin detecte as good");
-            Assert.AreEqual(-1, lastCoinValue, "Invalid coin registered a value");
-
 
         }
 
